84ec5da215bab6dec5a5fdab1156996078a2c058
HCThomas <hthomas1215@gmail.com>
2021-05-25 00:56:29 -0400

    change terminal to st

--- config.def.h
+++ config.def.h
@@ -22,7 +22,7 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"alacritty", "-t", "spterm", NULL };
+const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {"keepassxc", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
@@ -38,10 +38,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",	  NULL,			NULL,		0,				1,			 -1 },
-	{ "Firefox",  NULL,			NULL,		1 << 8,			0,			 -1 },
-	{ NULL,		  "spterm",		NULL,		SPTAG(0),		1,			 -1 },
-	{ NULL,		  "keepassxc",	NULL,		SPTAG(1),		0,			 -1 },
+	{ "Gimp",  	NULL,		NULL,		0,			1,			 -1 },
+	{ "Firefox",  	NULL,		NULL,		1 << 8,			0,			 -1 },
+	{ NULL,	  	"spterm",	NULL,		SPTAG(0),		1,			 -1 },
+	{ NULL,		"keepassxc",	NULL,		SPTAG(1),		0,			 -1 },
 };
 
 /* layout(s) */
